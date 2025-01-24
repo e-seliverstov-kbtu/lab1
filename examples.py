0edebe8037546fb76bb1c7994313b2a6be8046a1
Egor <e_seliverstov@kbtu.kz>
2025-01-24 22:07:46 +0500

    add examples

---
+++ examples.py
@@ -0,0 +1,86 @@
+print("Hello, World!")
+
+import sys
+
+print(sys.version)
+
+if 5 > 2:
+  print("Five is greater than two!")
+
+
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+
+#This is a comment.
+print("Hello, World!")
+
+
+print("Hello, World!") #This is a comment
+
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+
+
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+
+
+x = "John"
+# is the same as
+x = 'John'
+
+a = 4
+A = "Sally"
+#A will not overwrite a
+
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
